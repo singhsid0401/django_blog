5633cd5f2bfd2b23b97b2dcd6f63ddf624eeeeca
sidrocks95 <sidrocks95@gmail.com>
2018-11-14 04:53:09 +0530

    Adding Blog Posts in DB using Post model

--- django_project/blog/models.py
+++ django_project/blog/models.py
@@ -1,6 +1,13 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 class Post(models.Model):
   title = models.CharField(max_length=100)
   content = models.TextField()
-  date_posted = models.DateTimeField()
+  date_posted = models.DateTimeField(default=timezone.now)
+  author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+  #Dunder to display objects by object title
+  def __str__(self):
+    return self.title
--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,25 +1,26 @@
 from django.shortcuts import render
+from .models import Post
 #from django.http import HttpResponse
 
-posts = [
-    {
-        'author': 'Sid',
-        'title': 'Blog post 1',
-        'content': 'First post content',
-        'date_posted': '27/07/2018'
-    },
-    {
-        'author': 'Mug',
-        'title': 'Blog post 2',
-        'content': 'Second post content',
-        'date_posted': '28/07/2018'
-    }
-]
+# posts = [
+#     {
+#         'author': 'Sid',
+#         'title': 'Blog post 1',
+#         'content': 'First post content',
+#         'date_posted': '27/07/2018'
+#     },
+#     {
+#         'author': 'Mug',
+#         'title': 'Blog post 2',
+#         'content': 'Second post content',
+#         'date_posted': '28/07/2018'
+#     }
+# ]
 
 def home(request):
     #return HttpResponse('<h1>Blog Home</h1>')
     context={
-        'posts': posts
+        'posts': Post.objects.all() #Can also pass list of dictionaries here
     }
     return render(request, 'blog/home.html', context)
 
