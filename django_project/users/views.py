59e7e8c44d8eb845a291c9228594bcdeec99b9ff
sidrocks95 <sidrocks95@gmail.com>
2019-04-02 02:59:21 +0530

    Added user app with functionalities like User Profile

--- django_project/users/views.py
+++ django_project/users/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 #from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from .forms import UserRegisterForm
+from django.contrib.auth.decorators import login_required
 
 def register(request):
     if request.method == 'POST':
@@ -9,8 +10,13 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
-            return redirect('blog-home')
+            #Flash messages - disappears after one time
+            messages.success(request, f'Your account has been created. Try Logging in!')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
