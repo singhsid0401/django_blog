f12363a89e5c0e05a27c8f3ecbc2b1d4d1c4a993
sidrocks95 <sidrocks95@gmail.com>
2019-04-03 10:23:00 +0530

    User Profile

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -2,20 +2,21 @@ from django.shortcuts import render
 from .models import Post
 #from django.http import HttpResponse
 
-# posts = [
-#     {
-#         'author': 'Sid',
-#         'title': 'Blog post 1',
-#         'content': 'First post content',
-#         'date_posted': '27/07/2018'
-#     },
-#     {
-#         'author': 'Mug',
-#         'title': 'Blog post 2',
-#         'content': 'Second post content',
-#         'date_posted': '28/07/2018'
-#     }
-# ]
+""" posts = [
+    {
+         'author': 'Sid',
+         'title': 'Blog post 1',
+         'content': 'First post content',
+         'date_posted': '27/07/2018'
+     },
+     {
+         'author': 'Mug',
+         'title': 'Blog post 2',
+         'content': 'Second post content',
+         'date_posted': '28/07/2018'
+     }
+] """
+
 
 def home(request):
     #return HttpResponse('<h1>Blog Home</h1>')
